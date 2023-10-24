fa01b00b24b30f78513fb72c00f4b171c3a81931
ameywtf <ameydhurgude007@gmail.com>
2023-10-24 21:10:57 +0530

    debug ga

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import ReactDOM from "react-dom/client";
 
+import ReactGA from "react-ga";
+
 import { ReactFlowProvider } from "reactflow";
 
 import { ChakraProvider } from "@chakra-ui/react";
@@ -15,6 +17,10 @@ import "./index.css";
 export const MIXPANEL_TOKEN = import.meta.env.VITE_MIXPANEL_TOKEN;
 
 if (MIXPANEL_TOKEN) mixpanel.init(MIXPANEL_TOKEN);
+
+ReactGA.initialize("G-SYWPFMV140");
+
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
